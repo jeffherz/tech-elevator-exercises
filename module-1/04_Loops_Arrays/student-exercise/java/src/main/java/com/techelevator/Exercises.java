b3225ba5d2bdaecb969c59b99fe9db22f8fd25c8
Jeff Herz (JJ) <66926205+jeffherz@users.noreply.github.com>
2021-03-09 14:47:41 -0500

    Module 1 Exercise 4 completed

--- module-1/04_Loops_Arrays/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -10,6 +10,9 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
+		if (nums[0] == 6 || nums[nums.length-1] == 6) {
+			return true;
+		}
 		return false;
 	}
 
@@ -21,6 +24,9 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
+		if (nums.length >= 1 && nums[0] == nums[nums.length-1]) {
+			return true;
+		}
 		return false;
 	}
 
@@ -29,7 +35,7 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		return new int[] {3, 1, 4};
 	}
 
 	/*
@@ -40,6 +46,9 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
+		if (a[0] == b[0] || a[a.length-1] == b[b.length-1]) {
+			return true;
+		}
 		return false;
 	}
 
@@ -50,7 +59,11 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		return sum;
 	}
 
 	/*
@@ -61,7 +74,12 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+		int[] newArray = new int[nums.length];
+		newArray[nums.length-1] = nums[0];
+		for (int i = 0; i < nums.length-1; i++) {
+			newArray[i] = nums[i+1];
+		}
+		return newArray;
 	}
 
 	/*
@@ -72,7 +90,11 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+		int[] newArray = new int[nums.length];
+		for (int i = nums.length - 1, j = 0; i >= 0; i--, j++) {
+			newArray[j] = nums[i];
+		}
+		return newArray;
 	}
 
 	/*
@@ -83,7 +105,17 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+		int largestValue = 0;
+		int[] newArray = new int[nums.length];
+		if (nums[0] >= nums[nums.length-1]) {
+			largestValue = nums[0];
+		} else {
+			largestValue = nums[nums.length-1];
+		}
+		for (int i = 0; i < nums.length; i++) {
+			newArray[i] = largestValue;
+		}
+		return newArray;
 	}
 
 	/*
@@ -94,7 +126,11 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+		int sum = 0;
+		for (int i = 0; i < nums.length && i < 2; i++) {
+			sum += nums[i];
+		}
+		return sum;
 	}
 
 	/*
@@ -105,7 +141,7 @@ public class Exercises {
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+		return new int[] {a[1], b[1]};
 	}
 
 	/*
@@ -116,7 +152,13 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int evenNumberCount = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 2 == 0) {
+				evenNumberCount++;
+			}
+		}
+		return evenNumberCount;
 	}
 
 	/*
@@ -148,6 +190,11 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+		for (int i = 0; i < nums.length-1; i++) {
+			if (nums[i] == 2 && nums[i+1] == 2) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -158,7 +205,12 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
-		return false;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1 || nums[i] == 3) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/*
@@ -168,7 +220,17 @@ public class Exercises {
 	 sum28([1, 2, 3, 4]) → false
 	 */
 	public boolean sum28(int[] nums) {
-		return false;
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if (nums[i] == 2) {
+				sum += 2;
+			}
+		}
+		if (sum == 8) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
