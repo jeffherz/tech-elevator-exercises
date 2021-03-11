bd11605e381d122ad2bbc91f3d1205d1573d4095
Jeff Herz (JJ) <66926205+jeffherz@users.noreply.github.com>
2021-03-11 15:15:11 -0500

    11_Inheritance exercises complete (all tests pass)

---
+++ module-1/11_Inheritance/student-exercise/java/src/main/java/com/techelevator/CheckingAccount.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+public class CheckingAccount extends BankAccount{
+
+	public CheckingAccount(String accountHolderName, String accountNumber) {
+		super(accountHolderName, accountNumber);
+	}
+	public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
+		super(accountHolderName, accountNumber, balance);
+	}
+
+	// override methods
+	
+	@Override
+	public int withdraw(int amountToWithdraw){
+		if (getBalance() - amountToWithdraw >= -100.00) {
+			super.withdraw(amountToWithdraw);
+			if(getBalance() < 0) {
+				super.withdraw(10);
+			}
+		}
+		return getBalance();
+	}
+
+}
